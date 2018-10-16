40ad5113bbc389b058c2a7fb357481f7091f849b
aacc1110 <tadrow@daum.net>
2018-10-16 17:14:58 +0900

    idea

--- first/js/menulock.js
+++ first/js/menulock.js
@@ -1,14 +1,28 @@
-var m = document.querySelector("main"),
+(function() {
+
+  'use strict';
+  
+  var m = document.querySelector("main"),
     h = document.querySelector("header"),
     hHeight;
- 
-function setTopPadding() {
-  hHeight = h.offsetHeight;
-  m.style.paddingTop = hHeight + "px";
-}
-function onScroll() {
-    window.addEventListener("scroll", callbackFunc);
+  
+  function setTopPadding() {
+    hHeight = h.offsetHeight;
+    m.style.paddingTop = hHeight + "px";
+  }
+  
+  function onScroll() {
+  
+    window.addEventListener("scroll", _.throttle(callbackFunc, 200));
+    /**
+     * Replace the line above with the following one 
+     * to see how many more times the "callbackFunc" is executed
+     */
+    // window.addEventListener("scroll", callbackFunc);
+  
     function callbackFunc() {
+      c.style.visibility = "visible";
+      c.innerHTML = counter++;
       var y = window.pageYOffset;
       if (y > 150) {
         h.classList.add("scroll");
@@ -17,11 +31,13 @@ function onScroll() {
       }
     }
   }
+  
   window.onload = function() {
     setTopPadding();
     onScroll();
   };
-   
+  
   window.onresize = function() {
     setTopPadding();
-  };  
+  };
+  })();
