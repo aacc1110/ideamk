26052b7723f76583a04e8566bc25b413810b9c7e
aacc1110 <tadrow@daum.net>
2018-10-19 06:12:41 +0900

    idea

--- ideamk/js/wleh.js
+++ ideamk/js/wleh.js
@@ -190,7 +190,7 @@ function displayPagination(pagination) {
             el.onclick = (function(i) {
                 return function() {
                     pagination.gotoPage(i);
-                }
+                };
             })(i);
         }
 
