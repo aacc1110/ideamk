2b30f9d7e88c02193ab90d3e001a346728cbd9a8
aacc1110 <tadrow@daum.net>
2018-10-25 19:21:12 +0900

    idea

---
+++ ideamk/nodejs/syntax/comparison.js
@@ -0,0 +1,9 @@
+console.log(1==1); //true (==)는 좌항과 우항이 같은가
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //true
+console.log(1===1); //true (===)는 좌항과 우항이 정확하게 일치하는가
+console.log(1===2); //false
+name = 1;
+// (=) 대입연산자, (==) 비교연산자, (==)보다(===)를 써라
+
---
+++ ideamk/nodejs/syntax/template.js
@@ -0,0 +1,7 @@
+var name = 'k885j';
+var letter = 'News from My Lad by James Campbell, 1858-1859 (Walker Art Gallery)s written message to another pertaining to some matter of common concern.[1] Letters have several different types: Formal letters and informal letters. Letters contribute to the protection and conservation of literacy.[1] Letters have been sent since antiquity and are mentioned in the Iliad.[2] Both Herodotus and Thucydides mention letters in their histories.[3]';
+
+var letter = `News from ${name} 
+
+My Lad by James Campbell, 1858-1859 (Walker Art Gallery)s written message to another pertaining to some matter of common concern.[1] Letters have several different types: Formal letters and informal letters. Letters contribute to the protection and conservation of literacy.[1] Letters have been sent since antiquity and are mentioned in the Iliad.[${name}] Both Herodotus and Thucydides mention letters in their histories.[3]`;
+console.log(letter);
