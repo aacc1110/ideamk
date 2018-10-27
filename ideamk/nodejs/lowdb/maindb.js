0a056cc65326edbca055ec9f723412e2710b1607
aacc1110 <tadrow@daum.net>
2018-10-27 20:05:49 +0900

    idea

---
+++ ideamk/nodejs/lowdb/maindb.js
@@ -0,0 +1,33 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ topic: [], author: [],}).write();
+// db.get('author').push({
+//     id: 1,
+//     name: 'egoing',
+//     profile: 'developer'
+// }).write();
+// db.get('topic').push({
+//     id:1,
+//     title: 'lowdb',
+//     description: 'lowdb is ...',
+//     author: 1
+// }).write();
+// db.get('topic').push({
+//     id:2,
+//     title: 'mysql',
+//     description: 'mysql is ...',
+//     author: 1
+// }).write();
+
+// console.log(db.get('topic')
+// .find({title: 'lowdb', author:1})
+// .value()
+// );
+
+// db.get('topic').find({id:2}).assign({title: 'MySQL & MariaDB'}).write();
+
+// db.get('topic').remove({id: 2}).write();
+db.get().remove('topics').write();
